c5a451ea02efc811634d67779b9e0b034a2972fd
selvamani <selvamanib986@gmail.com>
2026-07-23 17:28:32 +0530

    test

--- client/src/Basic/components/ActiveTabList/index.jsx
+++ client/src/Basic/components/ActiveTabList/index.jsx
@@ -184,6 +184,7 @@ const ActiveTabList = () => {
     "ITEM SUB GROUP MASTER": <ItemSubGroupMaster />,
     "MATERIAL ISSUE": <MaterialIssue />,
     "MATERIAL RETURN": <MaterialReturn />,
+    
 
 
   };
